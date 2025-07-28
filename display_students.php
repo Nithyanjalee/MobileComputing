6ca1ae01fa0dfebf1f2052d3e58e299bd00a9894
Nithyanjalee <as2021513@sci.sjp.ac.lk>
2025-07-28 13:59:20 +0530

    Add display_students.php to show a list of students from the database

---
+++ display_students.php
@@ -0,0 +1,40 @@
+<?php 
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'studentdatabase';
+$connect = mysqli_connect($host, $user, $password, $database);
+
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT StudentID, FirstName, LastName, Height FROM student";
+$result = mysqli_query($connect, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<h2>Student List</h2>";
+    echo "<table border='1' cellpadding='8' cellspacing='0'>";
+    echo "<tr>
+            <th>Student ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Height (cm)</th>
+          </tr>";
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <td>{$row['StudentID']}</td>
+                <td>{$row['FirstName']}</td>
+                <td>{$row['LastName']}</td>
+                <td>{$row['Height']}</td>
+              </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No students found.";
+}
+
+mysqli_close($connect);
+?>
